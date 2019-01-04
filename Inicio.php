7c3bacab036f5f85cf3ae015dbcda2f2237cd462
Henrique Filho <hbaf71@gmail.com>
2019-01-04 03:20:44 -0200

    placar rowspan = 2

--- Inicio.php
+++ Inicio.php
@@ -67,16 +67,15 @@
                         //Nome do técnico
                         echo "<tr><th colspan=3 rowspan=1>Técnico: " . utf8_encode($row["tecnico"]) . "</th></tr>"; 
                     }else{
-                        echo "<tr><th colspan=1 rowspan=2>" . utf8_encode($row["id"]). "</th>" ."<th colspan=1 style='background-color: white; border-bottom:  1px solid black'><img src=index_files/Botafogo.png width=70 height=70 alt=Imagem /></th>"."<th colspan=1>Botafogo</th>"."<th colspan=1>" . utf8_encode($row["golsBotafogo"]) ." x ". utf8_encode($row["golsAdversario"]) . "</th>". "<th colspan=1 style='background-color: white; border-bottom:  1px solid black'><img src=index_files/$escudo.png width=70 height=70 alt=Imagem /></th>" ."<th colspan=1>"
+                        echo "<tr><th colspan=1 rowspan=2>" . utf8_encode($row["id"]). "</th>" ."<th colspan=1 style='background-color: white; border-bottom:  1px solid black'><img src=index_files/Botafogo.png width=70 height=70 alt=Imagem /></th>"."<th colspan=1>Botafogo</th>"."<th colspan=1 rowspan=2>" . utf8_encode($row["golsBotafogo"]) ." x ". utf8_encode($row["golsAdversario"]) . "</th>". "<th colspan=1 style='background-color: white; border-bottom:  1px solid black'><img src=index_files/$escudo.png width=70 height=70 alt=Imagem /></th>" ."<th colspan=1>"
                             . utf8_encode($row["adversario"]). "</th>"
                             ."<th colspan=1>" . utf8_encode($row["campeonato"]) . "</th>"
                             ."<th colspan=1>" . $date -> format( 'd-m-Y' ) . "</th>"
                             ."<th colspan=1>" . utf8_encode($row["estadio"]) . "</th>"."</tr>";
                         //Mostrando jogadores que fizeram gol de acordo com o número de gols
-                        echo "<tr><th colspan=2 style='text-align:left; vertical-align: text-top;'>". utf8_encode($row["autorBotafogo"]) ."</th><th colspan=1 style=border:black></th><th colspan=2 style='text-align:left; vertical-align: text-top;'>". utf8_encode($row["autorAdversario"]) ."</th>";
+                        echo "<tr><th colspan=2 style='text-align:left; vertical-align: text-top;'>". utf8_encode($row["autorBotafogo"]) ."</th><th colspan=2 style='text-align:left; vertical-align: text-top;'>". utf8_encode($row["autorAdversario"]) ."</th>";
                         //Nome do técnico
                         echo "<th colspan=3>Técnico: " . utf8_encode($row["tecnico"]) . "</th></tr>";
-                        
                     }
                 }
                 echo "</table>";
