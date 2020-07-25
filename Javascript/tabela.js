11b9fb64bfcd8c33b263178cc252921e7cd84393
Henrique Filho <hbaf71@gmail.com>
2020-07-24 22:11:17 -0300

    pesquisa adversario

--- Javascript/tabela.js
+++ Javascript/tabela.js
@@ -2,8 +2,8 @@ var jogos = [];
 
 var jogo1 = [true, "Flamengo", 5, 0, "2020-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
 var jogo2 = [false, "Santos", 5, 0, "2019-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
-var jogo3 = [true, "flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
-var jogo4 = [false, "flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
+var jogo3 = [true, "Flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
+var jogo4 = [false, "Flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
 
 jogos.push(jogo1);
 jogos.push(jogo2);
@@ -22,11 +22,21 @@ function decrescente(){
 	}
 }
 
-function escolheData(){
-	var valor = document.getElementById('dataJogo').value;
+function adversario(){
+	var adversario = document.getElementById('adversarioJogo').value;
 
 	for(var i = 0; i < jogos.length; i++){
-		if(valor == jogos[i][4]){
+		if(adversario == jogos[i][1]){
+			escreveLinha(jogos[i]);
+		}
+	}
+}
+
+function escolheData(){
+	var data = document.getElementById('dataJogo').value;
+
+	for(var i = 0; i < jogos.length; i++){
+		if(data == jogos[i][4]){
 			escreveLinha(jogos[i]);
 		}
 	}
