fb0bc88545a0316f15478fb732333bfd9ff8bd94
Henrique Filho <hbaf71@gmail.com>
2020-07-24 21:41:30 -0300

    pesquisas por data, crescente e decrescente funcionando

--- Javascript/tabela.js
+++ Javascript/tabela.js
@@ -3,18 +3,28 @@ var jogos = [];
 var jogo1 = [true, "Flamengo", 5, 0, "2020-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
 var jogo2 = [false, "Santos", 5, 0, "2019-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
 var jogo3 = [true, "flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
+var jogo4 = [false, "flamengo", 5, 0, "2050-08-31", "nilton santos", "série a", "paulo autuori", "Honda (5x)", ""];
 
 jogos.push(jogo1);
 jogos.push(jogo2);
 jogos.push(jogo3);
+jogos.push(jogo4);
 
-for(var i = 0; i < jogos.length; i++){
-	escreveLinha(jogos[i]);
+function crescente(){
+	for(var i = 0; i < jogos.length; i++){
+		escreveLinha(jogos[i]);
+	}
+}
+
+function decrescente(){
+	for(var i = jogos.length - 1; i >= 0; i--){
+		escreveLinha(jogos[i]);
+	}
 }
 
 function escolheData(){
 	var valor = document.getElementById('dataJogo').value;
-	
+
 	for(var i = 0; i < jogos.length; i++){
 		if(valor == jogos[i][4]){
 			escreveLinha(jogos[i]);
