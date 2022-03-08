37511ae98046f12460639f8d0e6ddaa8bffddb8a
Henrique Filho <hbaf71@gmail.com>
2022-03-07 23:33:26 -0300

    botafogo 5 x 0 volta redonda

--- jogos.js
+++ jogos.js
@@ -439,9 +439,9 @@ var jogo20220223 = ["Botafogo", "Flamengo", 1, 3, "Taça Guanabara", "2022-02-23
 jogos.push(jogo20220223);
 var jogo20220227 = ["Portuguesa-RJ", "Botafogo", 5, 3, "Taça Guanabara", "2022-02-27", "Luso Brasileiro", "Lúcio Flávio", ["Joel Carli (GC)","Watson","Bruno Santos","Miller","João Paulo"], ["Erison","Matheus Nascimento (2x)"], "https://www.youtube.com/embed/YQtY21nezcw", false];
 jogos.push(jogo20220227);
-/* var jogo20220307 = ["Botafogo", "Volta Redonda", 0, 0, "Taça Guanabara", "2022-03-07", "Nilton Santos", "Lúcio Flávio", [""], [""], "", false];
+var jogo20220307 = ["Botafogo", "Volta Redonda", 5, 0, "Taça Guanabara", "2022-03-07", "Nilton Santos", "Lúcio Flávio", ["Joel Carli", "Lucas Mezenga", "Rikelmi", "Erison (2x)"], [""], "", false];
 jogos.push(jogo20220307);
-var jogo20220319 = ["Audax Rio", "Botafogo", 0, 0, "Taça Guanabara", "2022-03-19", "Nilton Santos", "Lúcio Flávio", [""], [""], "", false];
+/* var jogo20220319 = ["Audax Rio", "Botafogo", 0, 0, "Taça Guanabara", "2022-03-19", "Nilton Santos", "Lúcio Flávio", [""], [""], "", false];
 jogos.push(jogo20220319); */
 
 jogos.sort(function(a, b){
