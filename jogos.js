511591196fd964ddfcdd748c83d5b5788118d282
Henrique Filho <hbaf71@gmail.com>
2024-02-09 11:20:09 -0300

    flamengo 1 x 0 botafogo

--- jogos.js
+++ jogos.js
@@ -609,6 +609,8 @@ let jogo20240127 = ["Botafogo", "Sampaio Corrêa-RJ", 2, 0, "Carioca", "2024-01-
 jogos.push(jogo20240127);
 let jogo20240130 = ["Botafogo", "Portuguesa-RJ", 1, 1, "Carioca", "2024-01-30", "Nilton Santos", "Tiago Nunes", ["Tiquinho Soares"], ["Anderson Rosa"], "https://www.youtube.com/embed/-Hc3ufhYutc"];
 jogos.push(jogo20240130);
+let jogo20240207 = ["Flamengo", "Botafogo", 1, 0, "Carioca", "2024-02-07", "Nilton Santos", "Tiago Nunes", ["Léo Pereira"], [""], "https://www.youtube.com/embed/s4H-awSTG5g"];
+jogos.push(jogo20240207);
 
 jogos.sort(function (a, b) {
     return a[5] < b[5] ? -1 : a[5] > b[5] ? 1 : 0;
