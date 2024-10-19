10229277b93edbf93a47a170a796cb30a7b8b7df
Henrique Filho <hbaf71@gmail.com>
2024-10-19 15:59:42 -0300

    botafogo 1 x 1 criciuma

--- jogos.js
+++ jogos.js
@@ -681,6 +681,8 @@ let jogo20240921 = ["Fluminense", "Botafogo", 0, 1, "Série A", "2024-09-21", "M
 jogos.push(jogo20240921);
 let jogo20240925 = ["São Paulo", "Botafogo", 1, 1, "Libertadores", "2024-09-25", "Morumbi", "Artur Jorge", [["Calleri"], [""], "Pênaltis: (4)", "Luiz Gustavo", "Lucas Moura", "Igor Vinícius", "Luciano"], ["Thiago Almada", [""], "Pênaltis: (5)", "Tchê Tchê", "Danilo Barbosa", "Marçal", "Thiago Almada", "Matheus Martins"], "https://www.youtube.com/embed/cV-JdpYVjMA"];
 jogos.push(jogo20240925);
+let jogo20241018 = ["Botafogo", "Criciúma", 1, 1, "Série A", "2024-10-18", "Maracanã", "Artur Jorge", ["Tiquinho Soares"], ["Felipe Vizeu"], "https://www.youtube.com/embed/6WwA7nRyAac"];
+jogos.push(jogo20241018);
 
 jogos.sort(function (a, b) {
     return a[5] < b[5] ? -1 : a[5] > b[5] ? 1 : 0;
