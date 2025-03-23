e9bfb351e2a35169325ddc7ff3c24fab8af548d4
Henrique Filho <hbaf71@gmail.com>
2025-03-23 15:08:37 -0300

    botafogo 3 x 1 novorizontino

--- jogos.js
+++ jogos.js
@@ -721,6 +721,8 @@ let jogo20250223 = ["Vasco", "Botafogo", 1, 0, "Carioca", "2025-02-23", "São Ja
 jogos.push(jogo20250223);
 let jogo20250227 = ["Botafogo", "Racing", 0, 2, "Recopa", "2025-02-27", "Nilton Santos", "Cláudio Caçapa", [""], ["Zaracho","Zuculini"], "https://youtube.com/embed/9i1867wmvHA", false];
 jogos.push(jogo20250227);
+let jogo20250322 = ["Botafogo", "Novorizontino", 3, 1, "Amistoso", "2025-03-22", "Nilton Santos", "Renato Paiva", ["Alexander Barboza","Patrick de Paula (2x)"], ["Marlon"], "https://www.youtube.com/embed/qRk5OPKc8fs", false];
+jogos.push(jogo20250322);
 
 jogos.sort(function (a, b) {
     return a[5] < b[5] ? -1 : a[5] > b[5] ? 1 : 0;
