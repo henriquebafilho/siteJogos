29845a71d6319042f243295f7919ce9f885be0ae
Henrique Filho <hbaf71@gmail.com>
2025-08-04 09:58:29 -0300

    Update jogos.js

--- jogos.js
+++ jogos.js
@@ -751,6 +751,8 @@ let jogo20250726 = ["Botafogo", "Corinthians", 1, 1, "Série A", "2025-07-26", "
 jogos.push(jogo20250726);
 let jogo20250729 = ["Botafogo", "Red Bull Bragantino", 2, 0, "Série A", "2025-07-29", "Nilton Santos", "Davide Ancelotti", ["Álvaro Montoro","Alexander Barboza"], [""], "https://www.youtube.com/embed/OqNIOka3_oo", false];
 jogos.push(jogo20250729);
+let jogo20250803 = ["Botafogo", "Cruzeiro", 0, 2, "Série A", "2025-08-03", "Nilton Santos", "Davide Ancelotti", [""], ["Christian","Matheus Pereira"], "https://www.youtube.com/embed/aBQ2D0LV9wM", false];
+jogos.push(jogo20250803);
 
 jogos.sort(function (a, b) {
     return a[5] < b[5] ? -1 : a[5] > b[5] ? 1 : 0;
